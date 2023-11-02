e522ec7926e50ad9979eae1ab9925c48cb4a0b0b
CraZyphr <niladrihowladar2@gmail.com>
2023-11-02 21:05:47 +0530

    Made header and Routes to Home,About and Sign In

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import SignUp from './SignUp';
 import About from './About';
 import Profile from './Profile';
 import Home from './Home';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<Signin />} />

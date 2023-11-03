fb4dfbfe755b14eac124d6f6289d3e67c226392e
CraZyphr <niladrihowladar2@gmail.com>
2023-11-03 19:09:42 +0530

    Made User Router , Mongo Db & hashed password

--- api/index.js
+++ api/index.js
@@ -1,16 +1,31 @@
-import express  from "express";
-import mongoose from "mongoose"
-import dotenv from"dotenv"
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config()
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log('Connected To MongoDB Sir')
-}).catch((err)=>{
-    console.log(err)
-})
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+
+ 
+
+const app = express();
+app.use(express.json());
 
 
-const app = express()
-app.listen(3000,()=>{
-    console.log("Server is Running on Port 3000 Sir!" )
-})
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
+
+app.use('/API/user', userRouter);
+app.use('/API/auth', authRouter);
+
+

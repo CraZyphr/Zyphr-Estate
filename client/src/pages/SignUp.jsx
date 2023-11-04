3281e238fb97e85c464b59bae046ae87d7d3ab5e
CraZyphr <niladrihowladar2@gmail.com>
2023-11-04 22:10:36 +0530

    Made Change In Sign In UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -40,81 +40,47 @@ export default function SignUp() {
     }
   };
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-    <div
-      className="relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0"
-    >
+    <div className='p-3 max-w-lg mx-auto '>
+    <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+    <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+      <input
+        type='text'
+        placeholder='username'
+        className='border p-3 rounded-lg'
+        id='username'
+        onChange={handleChange}
+      />
+      <input
+        type='email'
+        placeholder='email'
+        className='border p-3 rounded-lg'
+        id='email'
+        onChange={handleChange}
+      />
+      <input
+        type='password'
+        placeholder='password'
+        className='border p-3 rounded-lg'
+        id='password'
+        onChange={handleChange}
+      />
+
+      <button
+        disabled={loading}
+        className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+      >
+        {loading ? 'Loading...' : 'Sign Up'}
+      </button>
       
-      <div className="flex flex-col justify-center p-8 md:p-14">
-        <span className="mb-3 text-4xl font-bold">Sign Up !</span>
-        <span className="font-light text-gray-400 mb-8">
-          Please enter your details
-        </span>
-        <form onSubmit={handleSubmit}>
-        <div className="py-4">
-          <span className="mb-2 text-md">Username</span>
-          <input
-            type="text"
-            className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
-            id="username"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="py-4">
-          <span className="mb-2 text-md">Email</span>
-          <input
-            type="email"
-            className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="py-4">
-          <span className="mb-2 text-md">Password</span>
-          <input
-            type="password"
-            id="passwpord"
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
-          />
-        </div>
-        <div className="flex justify-between w-full py-4">
-          <div className="mr-24">
-            <input type="checkbox" name="ch" id="ch" className="mr-2" />
-            <span className="text-md">Remember me</span>
-          </div>
-          
-        </div>
-        <button disabled={loading}
-          className="w-full bg-black text-white p-2 rounded-lg mb-6 hover:bg-white hover:text-black hover:border hover:border-gray-300"
-        >
-          
-          {loading ? 'Loading...' : 'Sign Up'}
-        </button>
-        <button
-          className="w-full border border-gray-300 text-md p-2 rounded-lg mb-6 hover:bg-black hover:text-white"
-        >
-          <p>Sign in with Google</p>
-        </button>
-        </form>
-        
-        <div className="text-center text-gray-400">
-          Have an account?
-          <Link to={"/sign-in"}>
-        <span className="font-bold text-slate-600">Sign In</span>
-          </Link>
-        </div>
-      </div>
-      
-      <div className="relative">
-        <img
-          src="https://i.postimg.cc/9QkCw3ds/Picsart-23-11-04-13-50-41-249.jpg"
-          alt="img"
-          className="w-[400px] h-full hidden rounded-r-2xl md:block object-cover"
-        />
-      </div>
+    </form>
+    <div className='flex gap-2 mt-5'>
+      <p>Have an account?</p>
+      <Link to={'/sign-in'}>
+        <span className='text-blue-700'>Sign in</span>
+      </Link>
     </div>
     {error && <p className='text-red-500 mt-5'>{error}</p>}
   </div>
+
   );
 }
